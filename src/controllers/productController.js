61955dec85d4e7c81fe6d100b68a44a4f42f0790
Patricio González Camilo <patricio.gonzalez.camilo@gmail.com>
2022-04-06 18:31:59 -0400

    Actualización productController.js
    Se implemento un método para filtrar los datos por un rango.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,8 +6,14 @@ const pool  = require('../database/mysql');
  * @param {*} res 
  */
 const getItems = async (req, res) => {
+    const LIMIT = 9;
+    const page = req.query.page;
 
-    const q = 'SELECT * FROM product LIMIT 10';
+    const start = (page - 1) * LIMIT;
+
+    console.log("page number => "+page);
+
+    const q = 'SELECT * FROM product LIMIT '+start+','+LIMIT+'';
 
     pool.query(q, (err, data) => {
         if (err) console.log(err);
@@ -22,7 +28,13 @@ const getItems = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const getItem = (req, res) => { }
+const getItem = (req, res) => { 
+    console.log(req.params.id);
+    const numberPage = req.query.page;
+    
+
+    res.send({page : numberPage});
+}
 
 /**
  * 
