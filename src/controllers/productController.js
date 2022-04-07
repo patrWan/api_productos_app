ee8d89bd4a81289bbcc2d649c74063d8a2d5e478
Patricio González Camilo <patricio.gonzalez.camilo@gmail.com>
2022-04-07 19:56:03 -0400

    implementación de las querys para la paginación.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,20 +6,31 @@ const pool  = require('../database/mysql');
  * @param {*} res 
  */
 const getItems = async (req, res) => {
-    const LIMIT = 9;
+    
     const page = req.query.page ? req.query.page : 1;
-
     const categoryName = req.query.category ? req.query.category : 'bebida energetica';
 
+    const LIMIT = 9;
     const start = (page - 1) * LIMIT;
 
-    const q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product, category WHERE category.name = "'+categoryName+'" AND product.category = category.id LIMIT '+start+','+LIMIT+'';
-
-    pool.query(q, (err, data) => {
+    let totalPages = 0;
+    pool.query('SELECT COUNT(product.id) AS numberOfProducts FROM product, category WHERE category.name = "'+categoryName+'" AND product.category = category.id', (err, data) => {
         if (err) console.log(err);
-        res.send(data);
-    });
+        
+        totalPages = Math.ceil(data[0].numberOfProducts / LIMIT);
 
+        const q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product, category WHERE category.name = "'+categoryName+'" AND product.category = category.id LIMIT '+start+','+LIMIT+'';
+
+        pool.query(q, (err, data) => {
+            if (err) console.log(err);
+            res.send({
+                numberOfPages : totalPages,
+                products : data,
+            });
+        });
+
+
+    });
     
 }
 
