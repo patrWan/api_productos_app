566fa068ac6df8ae8c9428183176735bd45f4226
Patricio González Camilo <patricio.gonzalez.camilo@gmail.com>
2022-04-10 16:40:04 -0400

    Update productController.js
    Se eliminaron consultas innecesarias.

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -9,26 +9,23 @@ const getItems = (req, res) => {
 
     const page = req.query.page ? req.query.page : 1;
     const categoryName = req.query.category ? req.query.category : 'bebida energetica';
-    const productName = req.query.product_name;
+    const orderByName = req.query.name ? req.query.name : 'ASC';
 
     const LIMIT = 9;
     const start = (page - 1) * LIMIT;
-    
-    var q = '';
-    if(req.query.page === '' && req.query.category === ''){
-        q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product WHERE product.name LIKE "%' + productName + '%"';
-    }else{
-        q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product, category WHERE category.name = "' + categoryName + '" AND product.category = category.id LIMIT ' + start + ',' + LIMIT + '';
-    }
 
-   
+    var q = '';
+    q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product, category WHERE category.name = "' + categoryName + '" AND product.category = category.id ORDER BY product.name '+orderByName+' LIMIT ' + start + ',' + LIMIT + '';
+
+
+
 
     let totalPages = 0;
     pool.query('SELECT COUNT(product.id) AS numberOfProducts FROM product, category WHERE category.name = "' + categoryName + '" AND product.category = category.id', (err, data) => {
         if (err) console.log(err);
         totalPages = data[0].numberOfProducts / LIMIT;
 
-        
+
         pool.query(q, (err, data) => {
             if (err) console.log(err);
             res.send({
@@ -49,8 +46,9 @@ const getItems = (req, res) => {
  */
 const getItemByName = (req, res) => {
     const productName = req.query.product_name;
-
-    const q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product WHERE product.name LIKE "%' + productName + '%"';
+    const orderByName = req.query.name ? req.query.name : 'ASC';
+    
+    const q = 'SELECT product.name, product.url_image, product.price, product.discount FROM product WHERE product.name LIKE "%' + productName + '%" ORDER BY product.name '+orderByName+'';
     pool.query(q, (err, data) => {
         if (err) console.log(err);
         res.send({
